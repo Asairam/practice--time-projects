f38a1bbaa3bad8ca4ea17fcc913d694effdc2f42
Sairam <31530047+Asairam@users.noreply.github.com>
2018-04-01 20:29:50 +0530

    angular4 using jquery property

--- angular small projects/file_upload & Getter-Setter & jquery/src/app/about/about.component.ts
+++ angular small projects/file_upload & Getter-Setter & jquery/src/app/about/about.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {dataservice} from '../shared/ser/xyz.service';
+import * as $ from 'jquery'; // npm install jquery save
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -12,6 +13,14 @@ export class AboutComponent implements OnInit {
   constructor(private router:Router,private dataservice:dataservice) { }
 
   ngOnInit() {
+    $(document).ready(function(){
+        $("#butani").click(function(){
+            var div = $("#anidiv");  
+            div.animate({left: '100px'}, "slow");
+            div.animate({fontSize: '5em'}, "slow");
+        });
+    });
+    
   }
 
  demo={id:1,name:'sairam'}
